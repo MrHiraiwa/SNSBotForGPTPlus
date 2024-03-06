692554a59e726fea35f3efa637fdc91849238ad6
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-03-06 11:41:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -149,16 +149,6 @@ https://trends.google.co.jp/trends/trendingsearches/realtime?geo=JP&category=all
     'TWEET2_MAX_CHARACTER_COUNT': '280',
     'TWEET2_OVERLAY_URL': ''
 }
-auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
-client = tweepy.Client(
-    consumer_key = API_KEY,
-    consumer_secret = API_KEY_SECRET,
-    access_token = ACCESS_TOKEN,
-    access_token_secret = ACCESS_TOKEN_SECRET
-)
 
 # Firestore クライアントの初期化
 try:
