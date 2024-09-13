a036725cfcb899c19be3804b19fedaa2aef4ce71
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-13 10:53:12 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -327,12 +327,15 @@ def generate_tweet(tweet_no, user_id, bot_reply, retry_count=0, public_img_url=[
             overlay_img = None
             combined_img = None
             if tweet_no == 'tweet1' and TWEET1_OVERLAY_ON == "True":
+                print(f"combined overlay image. tweet_no:{tweet_no}, TWEET1_OVERLAY_ON:{TWEET1_OVERLAY_ON}")
                 overlay_img = get_image_with_retry(tweet_overlay_url)
                 combined_img = overlay_transparent_image(base_img, overlay_img)
             elif tweet_no == 'tweet2' and TWEET2_OVERLAY_ON == "True":
+                print(f"combined overlay image. tweet_no:{tweet_no}, TWEET1_OVERLAY_ON:{TWEET1_OVERLAY_ON}")
                 overlay_img = get_image_with_retry(tweet_overlay_url)
                 combined_img = overlay_transparent_image(base_img, overlay_img)
             else:
+                print(f"not combined overlay image. tweet_no:{tweet_no}")
                 combined_img = base_img
             
             # オーバーレイされた画像をアップロード
