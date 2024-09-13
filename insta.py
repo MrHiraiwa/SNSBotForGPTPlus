3edd7910d50c5e8abcdf7c32a3f02baf04f48efc
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-13 10:16:23 +0900

    Update insta.py

--- insta.py
+++ insta.py
@@ -253,7 +253,7 @@ def generate_insta(user_id, bot_reply, public_img_url=[]):
         base_img = get_image_with_retry(public_img_url)
         overlay_img = None
         combined_img = None
-        if INSTA_OVERLAY_ON　== "True":
+        if INSTA_OVERLAY_ON == "True":
             overlay_img = get_image_with_retry(insta_overlay_url)
             combined_img = overlay_transparent_image(base_img, overlay_img)
         else
