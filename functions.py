41f7a9f00d71a57085bee41ed0355cadc3133aa8
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-09-24 11:31:55 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -187,7 +187,7 @@ def generate_image(CORE_IMAGE_TYPE, prompt, paint_prompt, user_id, PAINTING_ON):
     print(f"generate_image prompt:{prompt}")
     try:
         if CORE_IMAGE_TYPE == "Vertex":
-            image_model = ImageGenerationModel.from_pretrained(VERTEX_IMAGE_MODEL)
+            image_model = ImageGenerationModel.from_pretrained("imagen-3.0-generate-001")
             response = model.generate_images(
                 prompt=prompt,
                 number_of_images=1,
