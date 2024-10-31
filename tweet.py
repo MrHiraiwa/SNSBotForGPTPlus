fa71d5e308de54b09aba4eaa5fb29c983d27a393
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2024-10-31 18:09:43 +0900

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -179,6 +179,7 @@ def response_filter(bot_reply):
     pattern208 = r"Source:"
     pattern209 = r"Learn more! 👉"
     pattern210 = r"Learn more:"
+    pattern211 = r"＃"
 
     # パターンに基づいてテキストをフィルタリング
     bot_reply = re.sub(pattern101, r" \1", bot_reply).strip()
@@ -221,6 +222,7 @@ def response_filter(bot_reply):
     bot_reply = re.sub(pattern208, " ", bot_reply).strip()
     bot_reply = re.sub(pattern209, " ", bot_reply).strip()
     bot_reply = re.sub(pattern210, " ", bot_reply).strip()
+    bot_reply = re.sub(pattern211, "#", bot_reply).strip()
     response = re.sub(r"\n{2,}", "\n", bot_reply)
 
     return response.rstrip('\n')
