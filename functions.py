9dbc706838a2a31c0d8d784067f12ab2fb6869ca
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2025-01-06 17:54:26 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -206,7 +206,7 @@ def generate_image(CORE_IMAGE_TYPE, prompt, paint_prompt, user_id, PAINTING_ON):
                 language="ja",
                 seed=None,
                 safety_filter_level="block_some",
-                person_generation="allow_adult",
+                person_generation="allow_all",
             )
             print(f"Vertex image response:{response}")
             image_result = save_image_locally(response[0])
